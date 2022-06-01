cf17bb244661eb02a20d93843476a0811478a5ed
Hadi Behmanesh <haadibehmanesh@gmail.com>
2022-06-01 20:01:56 +0200

    add movies and searchterm states , send request to api and get data and then map movies and setstates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import MovieCard from "./components/movieCard";
+import SearchIcon from './search.svg';
 
+const API_URL = `https://www.omdbapi.com?apikey=d311a810`;
 
 function App() {
-  return (
-    <>
 
-    </>
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    setMovies(data.Search);
+  }
+  useEffect(() => {
+    searchMovies(searchTerm);
+
+  }, [searchTerm])
+  return (
+    <div className="app">
+
+      <h1>Movie Land</h1>
+      <div className="search">
+        <input placeholder="Search for Movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={SearchIcon}
+          alt='search'
+          onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {
+        (movies || []).length > 0 ? movies.map((movie) => (
+          <MovieCard movie={movie} />
+        ))
+          : (
+            <div className="empty">No movies found</div>
+          )
+      }
+    </div>
   );
 }
 
